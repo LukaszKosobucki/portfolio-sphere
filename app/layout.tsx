e0dc04baec1ab9f15a0be38640fea6c56f310165
Lukasz Kosobucki <lukasz.kosobucki@yougov.com>
2024-10-17 18:12:29 +0200

    added SSR SEO from contentful

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,41 @@
 import { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
+import { fetchSpecificEntries } from "./services/contentful";
 
-export const metadata: Metadata = {
-  title: "Portfolio-sphere",
-  description: "Lukasz Kosobucki Portfolio based on astro-sphere",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const seoResponse = await fetchSpecificEntries({
+    content_type: "seoConfig",
+    title: "LK Portfolio",
+  });
+  const seoData = seoResponse[0].fields;
+
+  return {
+    metadataBase: new URL(`${seoData.canonicalUrl}`),
+    title: `${seoData.pageTitle}`,
+    description: `${seoData.seoDescription}`,
+    authors: {
+      name: `${seoData.author}`,
+    },
+    robots: `${seoData.canonicalUrl}`,
+    openGraph: {
+      title: `${seoData.pageTitle}`,
+      description: `${seoData.seoDescription}`,
+      images: [
+        {
+          url: `${seoData.seoImage}`,
+          alt: `${seoData.seoImage}`,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${seoData.pageTitle}`,
+      description: `${seoData.seoDescription}`,
+      images: `${seoData.seoImage}`,
+    },
+  };
+}
 
 export default async function RootLayout({
   children,
